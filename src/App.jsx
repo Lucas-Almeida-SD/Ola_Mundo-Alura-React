9d1953035d444d3d7f8e54da7739c0fedd7bc7fe
Lucas <lucas.almdiniz@gmail.com>
2024-10-22 21:27:47 -0400

    feat: Adicionado componentes Header e Banner na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+
+import Banner from './components/Banner';
 
 function App() {
   return (
-    <div className="App" />
+    <>
+      <Header />
+      <Banner />
+    </>
   );
 }
 
